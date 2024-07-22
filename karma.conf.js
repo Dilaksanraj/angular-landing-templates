597eda4c00e72f22f54e0b6832cf9847453eed02
Dilaksan <dilaksanraj510@gmail.com>
2024-07-22 11:36:40 +0200

    app name changed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-crash-course'),
+      dir: require('path').join(__dirname, './coverage/digimaax'),
       subdir: '.',
       reporters: [
         { type: 'html' },
